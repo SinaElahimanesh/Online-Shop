813260defb4b42062890e3f39d30bc839617145a
SinaElahimanesh <sinaaelahimanesh@gmail.com>
2022-02-18 20:20:49 +0330

    fix navbar items

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,15 +4,13 @@ import Categories from "../components/Categories";
 import Footer from "../components/Footer";
 import Gallery from "../components/Gallery";
 import Navbar from "../components/Navbar";
-import NavbarWeb from "../components/NavbarWeb";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      {/* <Navbar /> */}
-      <NavbarWeb />
+      <Navbar />
       <Slider />
       <Categories />
       <hr />
