209c7f2be00f5f0c77159114d9e37f275c958d34
SinaElahimanesh <sinaaelahimanesh@gmail.com>
2022-02-18 18:19:49 +0330

    fix logged in user and login, register

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -1,11 +1,15 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../requestMethods";
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 export const login = async (dispatch, user, path) => {
   dispatch(loginStart());
   try {
     const res = await publicRequest.post(path, user);
     if(res.status == 200) {
+      cookies.set('username', user.username, { path: '/' })
+      console.log('a', cookies.get('username')); 
       window.location.href = "/";
     }         
     // console.log(res.status)
